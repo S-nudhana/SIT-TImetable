fcaa0616378278dae7169d4150f5522c6fe26f51
Nudhana Sarutipaisan <nu37317@gmail.com>
2025-09-20 10:20:26 +0700

    first commit

--- TimeTable_Backend/src/lib/bcrypt.ts
+++ TimeTable_Backend/src/lib/bcrypt.ts
@@ -1,5 +1,7 @@
 import bcrypt from "bcrypt";
 
+import env from '../schemas/env.schema';
+
 export const compare = (password: string, queryPassword: string) => {
     return bcrypt.compareSync(password, queryPassword);
 };
@@ -8,6 +10,6 @@ export const hashPassword = (password: string) => {
     if (!process.env.SALT) {
         throw new Error("SALT is not defined in environment variables");
     }
-    const salt = bcrypt.genSaltSync(parseInt(process.env.SALT, 10));
+    const salt = bcrypt.genSaltSync(parseInt(env.SALT, 10));
     return bcrypt.hashSync(password, salt);
 };
